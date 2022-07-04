7eecf34fc62c759cf0b8bcf6cd82c15bc08b9d74
SteelPh0enix <steelphoenix2137@gmail.com>
2022-07-04 04:18:44 +0200

    modified tests for math, important: generate the data into CSV and load it to Pandas instead of making dataframes by hand.

--- math_tests.py
+++ math_tests.py
@@ -1,44 +1,33 @@
-from plots_utils import HistogramDataset, show_histograms_on_grid
 from math_utils import random_uniform_float, random_normal_float, add_noise
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def test_random_uniform_float():
-    numbers_a = [random_uniform_float(10) for _ in range(0, 10000)]
-    numbers_b = [random_uniform_float([10]) for _ in range(0, 10000)]
-    numbers_c = [random_uniform_float([10, 20]) for _ in range(0, 10000)]
-    numbers_d = [random_uniform_float((-20, -10)) for _ in range(0, 10000)]
+    numbers_a = [random_uniform_float(10) for _ in range(0, 10)]
+    numbers_b = [random_uniform_float([10]) for _ in range(0, 10)]
+    numbers_c = [random_uniform_float([10, 20]) for _ in range(0, 10)]
+    numbers_d = [random_uniform_float((-20, -10)) for _ in range(0, 10)]
 
-    show_histograms_on_grid(
-        [
-            HistogramDataset(values=numbers_a, label="A", bins=50),
-            HistogramDataset(values=numbers_b, label="B", bins=50),
-            HistogramDataset(values=numbers_c, label="C", bins=50),
-            HistogramDataset(values=numbers_d, label="D", bins=50),
-        ],
-        2,
-        2,
-        "Random uniform floats",
-    )
-
-
+    print("test_random_uniform_float")
+    print(numbers_a)
+    print(numbers_b)
+    print(numbers_c)
+    print(numbers_d)
+    
 def test_random_normal_float():
-    numbers_a = [random_normal_float() for _ in range(0, 10000)]
-    numbers_b = [random_normal_float(0, 10) for _ in range(0, 10000)]
-    numbers_c = [random_normal_float(10, 1) for _ in range(0, 10000)]
-    numbers_d = [random_normal_float(5, 5) for _ in range(0, 10000)]
+    numbers_a = [random_normal_float() for _ in range(0, 10)]
+    numbers_b = [random_normal_float(0, 10) for _ in range(0, 10)]
+    numbers_c = [random_normal_float(10, 1) for _ in range(0, 10)]
+    numbers_d = [random_normal_float(5, 5) for _ in range(0, 00)]
 
-    show_histograms_on_grid(
-        [
-            HistogramDataset(values=numbers_a, label="A", bins=50),
-            HistogramDataset(values=numbers_b, label="B", bins=50),
-            HistogramDataset(values=numbers_c, label="C", bins=50),
-            HistogramDataset(values=numbers_d, label="D", bins=50),
-        ],
-        2,
-        2,
-        "Random normal floats",
-    )
+    print("test_random_normal_float")
+    print(numbers_a)
+    print(numbers_b)
+    print(numbers_c)
+    print(numbers_d)
 
 
 def test_random_float():
@@ -47,15 +36,13 @@ def test_random_float():
 
 
 def test_add_noise():
-    numbers_args = np.linspace(0, 2 * np.pi, 10000)
+    numbers_args = np.linspace(0, 2 * np.pi, 10)
     numbers = [np.sin(x) for x in numbers_args]
-    noised_numbers = [add_noise(number, 0.0, 1.0) for number in numbers]
+    noised_numbers = [add_noise(number, 0.0, 0.1) for number in numbers]
 
-    # plot_lines_on_single_plot(
-    #     [(numbers_args, numbers), (numbers_args, noised_numbers)],
-    #     ["Original", "Noised"],
-    #     ["b", "r"],
-    # )
+    print("test_add_noise")
+    print(numbers)
+    print(noised_numbers)
 
 
 def test_numbers_generator():
